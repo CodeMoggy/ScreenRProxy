fd6994a7bc59e2c9b398c943e420eeb57d1d0572
Richard Custance <ricustan@microsoft.com>
2020-04-16 00:30:39 +0100

    changed partitionkey

--- ReadScanData.cs
+++ ReadScanData.cs
@@ -19,7 +19,7 @@ namespace ScanScreenProxy.Function
             // create table entry
             var item = new Participant()
             {
-                PartitionKey = myScanItem.ZipCode,
+                PartitionKey = myScanItem.Puma,
                 RowKey = Guid.NewGuid().ToString(),
                 ZipCode = myScanItem.ZipCode,
                 SurveyDate = DateTime.Now,
